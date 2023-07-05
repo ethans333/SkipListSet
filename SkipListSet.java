44a45bcf09e60e3b50bdb7623b93248b6818d0f2
Ethan <ethanmstn33@gmail.com>
2023-07-05 16:35:38 -0400

    Created function singatures for SkipListSet class

---
+++ SkipListSet.java
@@ -0,0 +1,138 @@
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.SortedSet;
+
+public class SkipListSet <T extends Comparable<T>> implements SortedSet<T> {
+
+    // Adds the specified element to this set if it is not already present.
+    public boolean add (T e) {
+        return false;
+    }
+
+    // Adds all of the elements in the specified collection to this set if they're not already present.
+    public boolean addAll (Collection<? extends T> c) {
+        return false;
+    }
+
+    // Removes all of the elements from this set. 
+    public void clear () {
+        return;
+    }
+
+    // Returns the comparator used to order the elements in this set, or null if this set uses the natural ordering of its elements.
+    public Comparator<? super T> comparator() {
+        return null;
+    }
+
+    // Returns true if this set contains the specified element.
+    public boolean contains (Object o) {
+        return false;
+    }
+
+    // Returns true if this set contains all of the elements of the specified collection.
+    public boolean containsAll (Collection<?> c) {
+        return false;
+    }
+
+    // Compares the specified object with this set for equality.
+    public boolean equals (Object o) {
+        return false;
+    }
+
+    // Returns the hash code value for this set.
+    public int hashCode () {
+        return -1;
+    }
+
+    // Returns true if this set contains no elements.
+    public boolean isEmpty () {
+        return false;
+    }
+
+    // Returns an iterator over the elements in this set.
+    public Iterator<T> iterator () {
+        return null;
+    }
+
+    // Removes the specified element from this set if it is present.
+    public boolean remove (Object o) {
+        return false;
+    }
+
+    // Removes from this set all of its elements that are contained in the specified collection.
+    public boolean removeAll (Collection<?> c) {
+        return false;
+    }
+
+    // Retains only the elements in this set that are contained in the specified collection.
+    public boolean retainAll (Collection<?> c) {
+        return false;
+    }
+
+    // Returns the number of elements in this set (its cardinality).
+    public int size () {
+        return 0;
+    }
+
+    // Returns a view of the portion of this set whose elements range from fromElement, inclusive, to toElement, exclusive.
+    public SkipListSet<T> subSet(T fromElement, T toElement) {
+        return null;
+    }
+
+    // Returns the first (lowest) element currently in this set.
+    public T first () {
+        return null;
+    }
+
+    // Returns the last (highest) element currently in this set.
+    public T last () {
+        return null;
+    }
+
+    // Returns a view of the portion of this set whose elements are strictly less than toElement
+    public SkipListSet<T> headSet (T toElement) {
+        return null;
+    }
+
+    // Returns a view of the portion of this set whose elements are greater than or equal to fromElement.
+    public SkipListSet<T> tailSet (T fromElement) {
+        return null;
+    }
+
+    // Returns an array containing all of the elements in this set.
+    public Object[] toArray () {
+        return null;
+    }
+
+    // SkipListSet Constructor
+    public SkipListSet () {
+
+    }
+
+    private class SkipListSetIterator<T extends Comparable<T>> implements Iterator<T> {  
+        // Returns true if the iteration has more elements.
+        public boolean hasNext () {
+            return false;
+        }
+
+        // Returns the next element in the iteration.
+        public T next () {
+            return null;
+        }
+
+        // Removes current element in iteration.
+        public void remove () {
+            return;
+        }
+    }
+
+    private class SkipListItem<T extends Comparable<T>> {
+        SkipListItem<T> up  = null;
+        SkipListItem<T> down = null;
+        SkipListItem<T> prev = null;
+        SkipListItem<T> next = null;
+
+        T payload = null;
+    }
+}
