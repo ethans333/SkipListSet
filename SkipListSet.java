579ae21ab8ca94f3afc623d7c9f3dfc70b13e396
Ethan <ethanmstn33@gmail.com>
2023-07-10 19:29:59 -0400

    Old insert created just saving for possible revert

--- SkipListSet.java
+++ SkipListSet.java
@@ -7,52 +7,18 @@ import java.util.Random;
 
 public class SkipListSet <T extends Comparable<T>> implements SortedSet<T> {
 
-    ArrayList<SkipListItem<T>> levels = new ArrayList<SkipListItem<T>>();
-    Random random = new Random();
+    private SkipListItem<T> head = null;
+    public int rows = 0;
 
-    // Generates skip list levels. The probability of a new level being generated decreases by 1/(2^i) every level
-    // head is new head to be at the head of every level
-    // Returns true if generating levels was successful, false otherwise
-    private boolean genLevels (SkipListItem<T> head) {
-        if (head == null) return false;
-
-        int bound = 2;
-
-        levels.add(head); // Adds at least one level
-        while (random.nextInt(bound-1) == 0) { // Probabilty of adding a new level decreases by 1/2
-            levels.add(head);
-            bound *= 2;
-        }
-
-        return true;
-    }
-
-    private boolean setNewHead (SkipListItem<T> item) {
-        for (int i = 0; i < levels.size(); i++) {
-            SkipListItem<T> oldHead = levels.get(i);
-
-            oldHead.prev = item; // Insert new head
-            item.next = oldHead;
-            levels.set(i, item);
-
-            if (random.nextInt((int)Math.pow(2, i))-1 != 0) // 1-[1/(2^i)] chance of old head getting deleted
-                levels.get(i).remove(item.next); // Remove old head
-        }
-        
-        return true;
-    }
+    private Random random = new Random();
 
     // Adds the specified element to this set if it is not already present.
     public boolean add (T e) {
-        SkipListItem<T> item = new SkipListItem<T>(e);
-        if (isEmpty()) genLevels(item);
-
-        if (levels.get(0).payload.compareTo(item.payload) < 0) // If item should be new head
-            return setNewHead(item);
-
-        for (int i = 0; i < levels.size(); i++)
-            if (random.nextInt((int)Math.pow(2, i))-1 == 0) // 1/(2^i) chance of getting inserted
-                levels.get(i).insert(item);
+        if (head == null) {
+            head = new SkipListItem<T>(e);
+            rows = head.nRows();
+            return true;
+        }
 
         return true;
     }
@@ -94,7 +60,7 @@ public class SkipListSet <T extends Comparable<T>> implements SortedSet<T> {
 
     // Returns true if this set contains no elements.
     public boolean isEmpty () {
-        return (levels.size() == 0);
+        return false;
     }
 
     // Returns an iterator over the elements in this set.
@@ -159,7 +125,6 @@ public class SkipListSet <T extends Comparable<T>> implements SortedSet<T> {
 
     // SkipListSet Constructor
     public SkipListSet () {
-
     }
 
     private class SkipListSetIterator<T extends Comparable<T>> implements Iterator<T> {  
@@ -179,74 +144,73 @@ public class SkipListSet <T extends Comparable<T>> implements SortedSet<T> {
         }
     }
 
-    public void print() {
-        for (int i = 0; i < levels.size(); i++) {
-            SkipListItem<T> current = levels.get(i);
-            System.out.printf("\n\nLevel: " + i + "\n");
-            while (current != null) {
-                if (current.payload instanceof Integer) {
-                    System.out.printf(((Integer)current.payload).intValue() + " -> ");
-                }
-                current = current.next;
-            }
-        }
-    }
-
-
     private class SkipListItem<T extends Comparable<T>> {
-        SkipListItem<T> prev = null;
-        SkipListItem<T> next = null;
+        private T value = null;
 
-        T payload = null;
+        ArrayList<SkipListItem<T>> prev = new ArrayList<SkipListItem<T>>();
+        ArrayList<SkipListItem<T>> next = new ArrayList<SkipListItem<T>>();
 
-        public SkipListItem (T payload) {
-            this.payload = payload;
+        private int r = 0; // current row
+
+        // Returns integer that has a 100% chance of being 1, 50% change of being 2, 100/2^n% of being n
+        public int nRows () {
+            int r = 1;
+            while (random.nextInt((int)Math.pow(2, r)) == 0)
+                r++;
+            return r;
         }
 
-        // Removes self from list
-        // item is the item to be deleted
-        // Returns what was removed from the list
-        // If current node is head of level does not update level's head
         public boolean remove (SkipListItem<T> item) {
-            if (payload.compareTo(item.payload) == 0) {
-                if (prev != null) prev.next = next;
-                if (next != null) next.prev = prev;
-                return true;
-            }
-
-            if (next == null) return false; // item doesn't exist in list
-
-            return next.remove(item);
+            return false;
         }
 
-        // Traverses linked list inserting item before, inbetween or after
-        // item is the item to be inserted
-        // Returns true if insertion was successful
-        // If current node was inserted to front does not update level's new head
-        public boolean insert (SkipListItem<T> item) {
-            if (payload.compareTo(item.payload) == 0) return true; // item already exists
+        // Recursively inserts item into skip list on a current row
+        // item is item to be inserted into row
+        // returns true if item inserted, false if item already exists in row
+        private boolean insertHelper (SkipListItem<T> item, int maxRow) {
+            if (value.compareTo(item.value) == 0) return false; // item already exists
 
-            if (payload.compareTo(item.payload) > 0) { // item < this
-                if (prev != null) prev.next = item; // Inserts new item behind current node
-                item.prev = prev;
-                prev = item;
-                return true;
-            } else if (next == null) { // Inserts new item after last node
-                next = item;
-                item.prev = this;
+            if (value.compareTo(item.value) < 0) {  // this < item
+                if (prev.get(r) == null) { // @ beginning of row
+                    T temp = value; // swap value w/ item.value then insert old value
+                    value = item.value;
+                    item.value = temp;
+                    return insert(item);
+                }
+
+                item.next.set(r, this); // insert in between
+                item.prev.set(r, prev.get(r));
+                prev.get(r).next.set(r, item);
+                prev.set(r, item);
                 return true;
             }
 
-            return next.insert(item);
+            if (next.size() == r) { // @ end of row
+                next.add(item); // insert at end
+                item.prev.add(this);
+                return true;
+            }
+
+            return next.get(r).insertHelper(item, maxRow); // continue to next node
+        }
+
+        // determines max row item will be inserted at and then inserts it into each row at or below max row
+        // item is item to be inserted into the skip list
+        // returns true if insertion was successful, false otherwise
+        public boolean insert (SkipListItem<T> item) {
+            int maxRow = Integer.MAX_VALUE;
+            while (maxRow > rows) maxRow = nRows(); // set max row to be row <= rows of skip list
+
+            for (r = rows; r >= 0; r--) insert(item, maxRow); // insert item at and below maxRow
+            return true;
+        }
+
+        public SkipListItem (T value) {
+            this.value = value;
         }
     }
 
     public static void main (String args[]) {
         SkipListSet<Integer> sls = new SkipListSet<Integer>();
-
-        for (int i = 0; i < 100; i++)
-            sls.add(new Integer(i));
-
-        sls.print();
     }
 }
